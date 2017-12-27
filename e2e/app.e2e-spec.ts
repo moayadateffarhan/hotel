c190e2a51e3b681d43450919a7486da94014e479
Moayad Farhan <mfarhan@cashU.local>
2017-12-27 22:12:36 +0200

    test

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { AppPage } from './app.po';
+import { AngularCliHerokuBoilerplatePage } from './app.po';
 
-describe('moayad App', () => {
-  let page: AppPage;
+describe('angular-cli-heroku-boilerplate App', () => {
+  let page: AngularCliHerokuBoilerplatePage;
 
   beforeEach(() => {
-    page = new AppPage();
+    page = new AngularCliHerokuBoilerplatePage();
   });
 
-  it('should display welcome message', () => {
+  it('should display message saying app works', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('app works!');
   });
 });
